4647dc9dc4d8fb8589e5b8a3e1ad6faa4d92a078
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2024-09-21 18:52:43 +0700

    fix: monster image rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'static.wikia.nocookie.net', // The domain you want to allow
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.wanderer.moe', // Another domain you want to allow
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ['static.wikia.nocookie.net', 'cdn.wanderer.moe'], // Add any additional domains you need here
+  },
+};
+
+export default nextConfig;
